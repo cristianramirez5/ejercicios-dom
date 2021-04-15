7dbb16fbda6e48a35178c4c58825ad70eb9813dc
Cristian Ramirez <ramirezcristian181@gmail.com>
2021-04-15 16:59:35 -0500

    scroll spy

--- js/scroll_espia.js
+++ js/scroll_espia.js
@@ -1,12 +1,27 @@
 const d = document;
 
 export default function scrollSpy() {
-    const $sections = d.querySelectorAll("section[data-scroll-spy]");
+  const $sections = d.querySelectorAll("section[data-scroll-spy]");
 
   const cb = (entries) => {
-    console.log("entries", entries);
+    //console.log("entries", entries);
+    entries.forEach((entry) => {
+      const id = entry.target.getAttribute("id");
+
+      if (entry.isIntersecting) {
+        d.querySelector(`a[data-scroll-spy][href="#${id}"]`).classList.add(
+          "active"
+        );
+      } else {
+        d.querySelector(`a[data-scroll-spy][href="#${id}"]`).classList.remove(
+          "active"
+        );
+      }
+    });
   };
 
-  const observer = new IntersectionObserver(cb, {});
-  $sections.forEach(el => observer.observe(el));
+  const observer = new IntersectionObserver(cb, {
+    threshold: [0.5,0.7]
+  });
+  $sections.forEach((el) => observer.observe(el));
 }
